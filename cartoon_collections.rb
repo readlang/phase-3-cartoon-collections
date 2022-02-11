4c35e4a029afbc461b02c14acb77bc737245aa99
Read Langworthy <rlangworthy@gmail.com>
2022-02-11 11:19:38 -0800

    commit tree badb4431450aa250766a7891a005b36802afb659

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,48 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (array)
+  array.each.with_index {|value, index| puts "#{index+1}. #{value}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet array
+  array.map {|x| x.capitalize + "!" } 
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+
+def long_planeteer_calls array
+  
+  return array.filter{ |x| x.length > 4 }.length > 0
+      
+
+
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+
+def find_the_cheese array
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  array.find do |word|
+    cheese_types.include?(word)
+  end
+
 end
+
+
+
+
+### call methods area:
+
+roll_call_dwarves ["Doc" , "Dopey", "Bashful", "Grumpy"]
+
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+p summon_captain_planet planeteer_calls
+
+
+p long_planeteer_calls ["earh", "wind", "fire", "watr", "hert"]
+
+
+p find_the_cheese ["tomato soup", "gouda", "cheddar", "oyster crackers", "gouda"]
